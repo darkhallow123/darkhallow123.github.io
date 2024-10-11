c9f730ecacf58b84717d96ae66dbde707c806704
anton_wallentin <111547361+darkhallow123@users.noreply.github.com>
2024-10-11 02:24:16 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add JavaScript interactivity if needed (e.g., smooth scrolling, dynamic loading)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
